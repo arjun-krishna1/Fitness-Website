904a3234636e8745f53338571e596791b16206f2
Mruhankhan <71145148+Mruhankhan@users.noreply.github.com>
2021-07-09 11:43:02 -0700

    Button and Js

--- public/work.js
+++ public/work.js
@@ -1,1 +1,11 @@
-// alert()
+let buttonSubmit = document.getElementById('button-submit')
+buttonSubmit.addEventListener('click', () =>{
+
+    let ageInput = document.getElementById('age')
+    let numberAgeInput = parseInt(ageInput)
+    let one = 1;
+    if(numberAgeInput === one){
+            alert()
+    }
+
+})
