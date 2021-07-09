1dcade6e04697170f675d67b336d7bda97cc9047
arjunkrishna <arjun.krishna@huawei.com>
2021-07-09 12:44:08 -0400

    improve spacing with css

--- index.js
+++ index.js
@@ -4,15 +4,19 @@ const app = express()
 
 
 app.use(express.static('public'))
-app.listen(process.env.PORT)
+
 app.set('view engine', 'ejs')
 
+const port  = process.env.PORT | 8000
+
+app.listen(port, () => {
+  console.log(`Server starting on port ${port}...`)
+}) 
+
 app.get('/', (req, res) =>{
-res.render('index')
+  res.render('index')
 })
 
 app.get('/results', (req, res) =>{
   res.render('results')
 })
-
-
